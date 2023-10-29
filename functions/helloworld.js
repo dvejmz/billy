4b3e7a9c072ea4161da00ccaf7e0e15d6eab51ef
David Jimenez <dvejmz@sgfault.com>
2023-10-29 16:45:57 +0000

    Retrieve D1 data from Pages Fn

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,11 @@
-export function onRequest(context) {
-  return new Response("Hello Cloudflare!");
+interface Env {
+  DB: D1Database;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  // Create a prepared statement with our query
+  const ps = context.env.DB.prepare('SELECT * from bill');
+  const data = await ps.first();
+
+  return Response.json(data);
 }
